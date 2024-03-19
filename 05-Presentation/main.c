d8ba024751e2467eb211e34577d29927b371699a
Lee Clark <wymux@shoash.com>
2024-03-19 00:55:55 +0000

    05-Presentataion: Return extensions and layers

--- 05-Presentation/main.c
+++ 05-Presentation/main.c
@@ -3,85 +3,96 @@
 #include <vulkan/vulkan.h>
 #include <vulkan/vulkan_core.h>
 
-int enumerateextensions()
+int enumerate_extensions(VkExtensionProperties **vk_extension_properties,
+			 uint32_t *extension_count)
 {
-	uint32_t extension_count = 0;
 	VkResult res = VK_SUCCESS;
-	res = vkEnumerateInstanceExtensionProperties(NULL, &extension_count,
+
+	res = vkEnumerateInstanceExtensionProperties(NULL, extension_count,
 						     NULL);
 	if (res != VK_SUCCESS) {
 		fprintf(stderr,
-			"Failed to acqoire extension count. Error code: %d\n",
+			"Failed to acquire extension count. Error code: %d\n",
 			res);
-		return 1;
-	}
-
-	VkExtensionProperties *vk_extension_properties =
-		malloc(sizeof(VkExtensionProperties) * extension_count);
-	if (!vk_extension_properties) {
-		fprintf(stderr, "Failed to allocate memory for extansions\n");
 		return 0;
 	}
 
-	res = vkEnumerateInstanceExtensionProperties(NULL, &extension_count,
-						     vk_extension_properties);
+	*vk_extension_properties =
+		malloc(sizeof(VkExtensionProperties) * (*extension_count));
+	if (!(*vk_extension_properties)) {
+		fprintf(stderr, "Failed to allocate memory for extensions\n");
+		return 0;
+	}
+
+	res = vkEnumerateInstanceExtensionProperties(NULL, extension_count,
+						     *vk_extension_properties);
 	if (res != VK_SUCCESS) {
 		fprintf(stderr,
 			"Failed to enumerate extensions: Error code: %d\n",
 			res);
-		free(vk_extension_properties);
+		free(*vk_extension_properties);
 		return 0;
 	}
 
-	printf("Available Vulkan Extensions\n");
-	for (uint32_t i = 0; i < extension_count; i++) {
-		printf("- %s (version %d)\n",
-		       vk_extension_properties[i].extensionName,
-		       vk_extension_properties[i].specVersion);
-	}
-
-	free(vk_extension_properties);
 	return 1;
 }
 
-int enumeratelayers()
+int enumerate_layers(VkLayerProperties **vk_layer_properties,
+		     uint32_t *layer_count)
 {
-	uint32_t layer_count = 0;
 	VkResult res = VK_SUCCESS;
 
-	res = vkEnumerateInstanceLayerProperties(&layer_count, NULL);
-	VkLayerProperties *vk_layer_properties =
-		malloc(sizeof(VkLayerProperties) * layer_count);
+	res = vkEnumerateInstanceLayerProperties(layer_count, NULL);
 	if (res != VK_SUCCESS) {
-		fprintf(stderr, "Failed to allocate memory\n");
+		fprintf(stderr,
+			"Failed to acquire layer count. Error code: %d\n", res);
 		return 0;
 	}
 
-	res = vkEnumerateInstanceLayerProperties(&layer_count,
-						 vk_layer_properties);
+	*vk_layer_properties =
+		malloc(sizeof(VkLayerProperties) * (*layer_count));
+	if (!(*vk_layer_properties)) {
+		fprintf(stderr, "Failed to allocate memory for layers\n");
+		return 0;
+	}
+
+	res = vkEnumerateInstanceLayerProperties(layer_count,
+						 *vk_layer_properties);
 	if (res != VK_SUCCESS) {
 		fprintf(stderr, "Failed to enumerate layers. Error code: %d\n",
 			res);
-		free(vk_layer_properties);
+		free(*vk_layer_properties);
 		return 0;
 	}
-
-	printf("Available Vulkan Layers:\n");
-	for (uint32_t i = 0; i < layer_count; i++) {
-		printf("- %s (version %d)\n", vk_layer_properties[i].layerName,
-		       vk_layer_properties[i].specVersion);
-		printf("  Description: %s\n",
-		       vk_layer_properties[i].description);
-	}
-
-	free(vk_layer_properties);
 	return 1;
 }
 
 int main()
 {
-	enumerateextensions();
-	enumeratelayers();
+	VkExtensionProperties *vk_extension_properties = NULL;
+	uint32_t extension_count = 0;
+	if (enumerate_extensions(&vk_extension_properties, &extension_count)) {
+		printf("Available Vulkan Extensions:\n");
+		for (uint32_t i = 0; i < extension_count; i++) {
+			printf("- %s (version %d)\n",
+			       (vk_extension_properties)[i].extensionName,
+			       (vk_extension_properties)[i].specVersion);
+		}
+	}
+
+	VkLayerProperties *vk_layer_properties = NULL;
+	uint32_t layer_count = 0;
+	if (enumerate_layers(&vk_layer_properties, &layer_count)) {
+		printf("Available Vulkan Layers:\n");
+		for (uint32_t i = 0; i < layer_count; i++) {
+			printf("- %s (version %d)\n",
+			       vk_layer_properties[i].layerName,
+			       vk_layer_properties[i].specVersion);
+			printf("  Description: %s\n",
+			       vk_layer_properties[i].description);
+		}
+	}
+
 	VkApplicationInfo vk_app_info = {};
 	vk_app_info.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
 	vk_app_info.pNext = NULL;
