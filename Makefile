9d8ad033cc79879fe6e5b68aa5bb92d2628a460e
Lee Clark <wymux@shoash.com>
2024-04-05 12:23:35 +0000

    Initiate triangle
    Signed-off-by: Lee Clark <wymux@shoash.com>

--- Makefile
+++ Makefile
@@ -1,11 +1,14 @@
 CC = gcc
-LDFLAGS = -lvulkan -lxcb
+LDFLAGS = -lvulkan -lxcb -lglfw -ldl -lpthread -lX11 -lXxf86vm -lXrandr -lXi
 
-SRC = 01-Introduction/main.c 02-Memory-and-Resources/main.c 03-Queues-and-Commands/main.c 04-Moving-Data/main.c 05-Presentation/main.c
+SRC = 01-Introduction/main.c 02-Memory-and-Resources/main.c 03-Queues-and-Commands/main.c 04-Moving-Data/main.c 05-Presentation/main.c triangle/triangle.c
 OBJ = $(SRC:.c=.o)
 BIN = $(SRC:.c=)
 
 all: format $(BIN)
+
+%.o: %.c
+	$(CC) $(CFLAGS) $(INCLUDE_PATHS) -c $< -o $@
 
 %: %.o
 	$(CC) $(CFLAGS) $< -o $@ $(LDFLAGS)
---
+++ triangle/triangle.c
@@ -0,0 +1,137 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
+
+#define GLFW_INCLUDE_VULKAN
+#include <GLFW/glfw3.h>
+
+typedef uint8_t u8;
+typedef uint16_t u16;
+typedef uint32_t u32;
+typedef int8_t i8;
+typedef int16_t i16;
+typedef int32_t i32;
+
+const char *WIN_TITLE = "SeEngine";
+const uint32_t WIN_WIDTH = 800;
+const uint32_t WIN_HEIGHT = 600;
+
+typedef struct App {
+	GLFWwindow *window;
+	VkInstance instance;
+} App;
+
+void initWindow(App *pApp);
+void initVulkan(App *pApp);
+void mainLoop(App *pApp);
+void cleanup(App *pApp);
+
+void createInstance(App *pApp);
+
+int main(void)
+{
+	App app = { 0 };
+
+	initWindow(&app);
+	initVulkan(&app);
+	mainLoop(&app);
+	cleanup(&app);
+	return 0;
+}
+
+void initWindow(App *pApp)
+{
+	glfwInit();
+
+	glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+	glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
+
+	pApp->window =
+		glfwCreateWindow(WIN_WIDTH, WIN_HEIGHT, WIN_TITLE, NULL, NULL);
+}
+
+void initVulkan(App *pApp)
+{
+	createInstance(pApp);
+}
+
+void mainLoop(App *pApp)
+{
+	while (!glfwWindowShouldClose(pApp->window)) {
+		glfwPollEvents();
+	}
+}
+
+void cleanup(App *pApp)
+{
+	vkDestroyInstance(pApp->instance, NULL);
+
+	glfwDestroyWindow(pApp->window);
+
+	glfwTerminate();
+}
+
+bool verfityExtensionSupport(u32 extensionCount,
+			     VkExtensionProperties *extensions,
+			     u32 glfwExtensionCount,
+			     const char **glfwExtensions)
+{
+	u32 foundExtensions = 0;
+	for (int i = 0; i < glfwExtensionCount; i++) {
+		for (int j = 0; j < extensionCount; j++) {
+			if (strcmp(extensions[j].extensionName,
+				   glfwExtensions[i]) == 0) {
+				foundExtensions++;
+			}
+		}
+	}
+
+	return foundExtensions == glfwExtensionCount;
+}
+
+void createInstance(App *pApp)
+{
+	VkApplicationInfo appInfo = {
+		.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO,
+		.pApplicationName = WIN_TITLE,
+		.applicationVersion = VK_MAKE_VERSION(1, 0, 0),
+		.pEngineName = "No Engine",
+		.engineVersion = VK_MAKE_VERSION(1, 0, 0),
+		.apiVersion = VK_API_VERSION_1_0,
+		.pNext = NULL
+	};
+
+	u32 glfwExtensionCount = 0;
+	const char **glfwExtensions;
+
+	glfwExtensions = glfwGetRequiredInstanceExtensions(&glfwExtensionCount);
+
+	VkInstanceCreateInfo createInfo = {
+		.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO,
+		.pApplicationInfo = &appInfo,
+		.enabledExtensionCount = glfwExtensionCount,
+		.ppEnabledExtensionNames = glfwExtensions
+	};
+	createInfo.enabledLayerCount = 0;
+
+	if (vkCreateInstance(&createInfo, NULL, &pApp->instance) !=
+	    VK_SUCCESS) {
+		printf("Failed to create Vulkan Instance\n");
+		exit(1);
+	}
+
+	u32 extensionCount = 0;
+	vkEnumerateInstanceExtensionProperties(NULL, &extensionCount, NULL);
+
+	VkExtensionProperties *extensions = (VkExtensionProperties *)malloc(
+		sizeof(VkExtensionProperties) * extensionCount);
+	vkEnumerateInstanceExtensionProperties(NULL, &extensionCount,
+					       extensions);
+
+	if (!verfityExtensionSupport(extensionCount, extensions,
+				     glfwExtensionCount, glfwExtensions)) {
+		printf("Missing extension support!\n");
+		exit(1);
+	}
+}
